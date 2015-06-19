7a5408da6c06a7715264d8bc7438ed916be79d27
kangjiastar <168568@123.com>
2015-06-19 14:48:47 +0800

    Revert "添加输出"
    This reverts commit 3f98c1d4a461e1e69d7329f2535f4f21ea150b26.

--- 01 - kang/ViewController.m
+++ 01 - kang/ViewController.m
@@ -16,8 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    NSLog(@"上传成功");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
