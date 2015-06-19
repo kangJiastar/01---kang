7c57115805d3fb9c0cb14405433f6f26e0104e15
kangjiastar <168568@123.com>
2015-06-19 15:12:46 +0800

    添加cell

---
+++ 01 - kang/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  01 - kang
+//
+//  Created by apple on 15/6/19.
+//  Copyright (c) 2015年 cloud7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ 01 - kang/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  01 - kang
+//
+//  Created by apple on 15/6/19.
+//  Copyright (c) 2015年 cloud7. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
